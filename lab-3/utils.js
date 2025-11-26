554904044b9323aba3284bc2bc625690d4a3d40a
vannishh <istvanishoff@yandex.ru>
2025-11-26 21:51:53 +0300

    1.0

---
+++ lab-3/utils.js
@@ -0,0 +1,24 @@
+function createImageBitmapFromFile(file) {
+  return new Promise((resolve, reject) => {
+    const url = URL.createObjectURL(file);
+    const img = new Image();
+    img.onload = () => { URL.revokeObjectURL(url); resolve(img); };
+    img.onerror = reject;
+    img.src = url;
+  });
+}
+
+function loadImageFromSrc(src) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.crossOrigin = "anonymous";
+    img.onload = () => resolve(img);
+    img.onerror = reject;
+    img.src = src;
+  });
+}
+
+function fitCanvasToImage(canvas, img) {
+  canvas.width = img.width;
+  canvas.height = img.height;
+}
